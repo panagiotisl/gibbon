b661035a78c2a6be71cdb851b43136c715dfddb4
Panagiotis Liakos <liakospanagiotis@gmail.com>
2025-08-05 12:18:54 +0300

    cleaned up code

--- src/main/java/fi/iki/yak/ts/compression/gorilla/ByteBufferBitInput.java
+++ src/main/java/fi/iki/yak/ts/compression/gorilla/ByteBufferBitInput.java
@@ -8,7 +8,7 @@ import java.nio.ByteBuffer;
  * @author Michael Burman
  */
 public class ByteBufferBitInput implements BitInput {
-    private ByteBuffer bb;
+    private final ByteBuffer bb;
     private byte b;
     private int bitsLeft = 0;
 
@@ -48,6 +48,34 @@ public class ByteBufferBitInput implements BitInput {
     @Override
 	public long getLong(int bits) {
         long value = 0;
+        while(bits > 0) {
+            if(bits > bitsLeft || bits == Byte.SIZE) {
+                // Take only the bitsLeft "least significant" bits
+                byte d = (byte) (b & ((1<<bitsLeft) - 1));
+                value = (value << bitsLeft) + (d & 0xFF);
+                bits -= bitsLeft;
+                bitsLeft = 0;
+            } else {
+                // Shift to correct position and take only least significant bits
+                byte d = (byte) ((b >>> (bitsLeft - bits)) & ((1<<bits) - 1));
+                value = (value << bits) + (d & 0xFF);
+                bitsLeft -= bits;
+                bits = 0;
+            }
+            flipByte();
+        }
+        return value;
+    }
+
+    /**
+     * Reads an integer from the next X bits that represent the least significant bits in the int value.
+     *
+     * @param bits How many next bits are read from the stream
+     * @return long value that was read from the stream
+     */
+    @Override
+    public int getInt(int bits) {
+        int value = 0;
         while(bits > 0) {
             if(bits > bitsLeft || bits == Byte.SIZE) {
                 // Take only the bitsLeft "least significant" bits
--- src/main/java/gr/aueb/delorean/gibbon/Value.java
+++ src/main/java/gr/aueb/delorean/gibbon/Value.java
@@ -7,13 +7,34 @@ package gr.aueb.delorean.gibbon;
  */
 public class Value {
     private int value;
+    private int diff;
+    private float epsilon;
 
     public Value(int value) {
         this.value = value;
+        this.diff = -1;
+    }
+
+    public Value(int value, int diff, float epsilon) {
+        this.value = value;
+        this.diff = diff;
+        this.epsilon = epsilon;
     }
 
     public float getFloatValue() {
         return Float.intBitsToFloat(value);
+        // switch (diff) {
+        //     case 0:
+        //         return Float.intBitsToFloat(value) + epsilon;
+        //     case 1:
+        //         return Float.intBitsToFloat(value) - epsilon;
+        //     case 2:
+        //         return Float.intBitsToFloat(value) + 2 * epsilon;
+        //     case 3:
+        //         return Float.intBitsToFloat(value) - 2 * epsilon;
+        //     default:
+        //         return Float.intBitsToFloat(value);
+        // }
     }
 
     public int getIntValue() {
